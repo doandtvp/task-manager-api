755b9d6921b958a5086ff51790ee4da697b39b50
doan <doandtvp@gmail.com>
2020-04-26 15:49:52 +0700

    restructuring sending email file

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,32 +3,20 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
-    const msg = {
+    sgMail.send({
         to: email,
         from: 'doandtvp@gmail.com',
         subject: 'Thanks for joining in!',
-        text: 'Welcome to the app, ' + name +'. Let me know more about you.'
-    }
-
-    sgMail.send(msg).then(() => {
-        console.log('Sending Email!')
-    }).catch((e) => {
-        console.log(e.response.body)
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
 }
 
 const sendCancelationEmail = (email, name) => {
-    const msg = {
+    sgMail.send({
         to: email,
         from: 'doandtvp@gmail.com',
-        subject: 'Sorry to see go!',
-        text: 'Goodbye, ' + name + '. I hope to see you back sometime soon!'
-    }
-
-    sgMail.send(msg).then(() => {
-        console.log('Succed!')
-    }).catch((e) => {
-        console.log(e.response.body)
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
 }
 
